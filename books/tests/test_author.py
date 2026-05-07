f6cab3a482bc25a16f52fe468f16af1e4d367ae2
Coder-Ankit001 <ankitchaudhary7157@gmail.com>
2026-05-07 20:25:53 +0000

    fix: Author testcase

--- books/tests/test_author.py
+++ books/tests/test_author.py
@@ -6,5 +6,5 @@ class AuthorTest(TestCase):
     # Test Creation of Author
     def test_create_author(self):
         author = Author.objects.create(name="Test Author", most_famous_book="Test Book")
-        self.assertEqual(author.count(), 1)
-        self.assertEqual(Author.objects.first().name, "Test Author")
+        self.assertEqual(author.name, "Test Author")
+        self.assertEqual(Author.objects.count(), 1)
