836ba4ba65b849813bd27e079e8d447f1cf19669
Coder-Ankit001 <ankitchaudhary7157@gmail.com>
2026-07-02 16:10:11 +0000

    Rearraged books.urls

--- books/urls.py
+++ books/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = "books"
 
 urlpatterns = [
+
     # Authors Endpoints
-    path("dahsboard/authors/", views.AuthorListView.as_view(), name="author_list"),
     path("authors/create/", views.AuthorCreateView.as_view(), name="author_create"),
     path(
         "authors/update/<int:pk>/",
@@ -22,16 +22,16 @@ urlpatterns = [
         views.AuthorDeleteView.as_view(),
         name="author_delete",
     ),
+
     # Books Endpoints
     path("books/", views.user_book_list, name="user_book_list"),
-    path("dashboard/books/", views.BookListView.as_view(), name="book_list"),
     path("books/create/", views.BookCreateView.as_view(), name="book_create"),
     path("books/update/<int:pk>/", views.BookUpdateView.as_view(), name="book_update"),
     path("books/detail/<int:pk>/", views.BookDetailView.as_view(), name="book_detail"),
     path("books/delete/<int:pk>/", views.BookDeleteView.as_view(), name="book_delete"),
+
     # Genres Endpoints
     path("genres/", views.user_genre_list, name="user_genre_list"),
-    path("dashboard/genres/", views.GenreListView.as_view(), name="genre_list"),
     path("genres/create/", views.GenreCreateView.as_view(), name="genre_create"),
     path(
         "genres/update/<int:pk>/", views.GenreUpdateView.as_view(), name="genre_update"
@@ -42,6 +42,12 @@ urlpatterns = [
     path(
         "genres/delete/<int:pk>/", views.GenreDeleteView.as_view(), name="genre_delete"
     ),
+
+    # dashboord endpoints
+    path("dashboard/books/", views.BookListView.as_view(), name="book_list"),
+    path("dashboard/genres/", views.GenreListView.as_view(), name="genre_list"),
+    path("dashboard/authors/", views.AuthorListView.as_view(), name="author_list"),
+
     # Search Boks
     path("search/", views.search_book, name="search_book"),
     path("search/genre/<int:pk>/", views.search_book_genre, name="search_book_genre"),
