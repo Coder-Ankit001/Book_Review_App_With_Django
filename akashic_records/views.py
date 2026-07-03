064f656db6cb4db5f61387cfd633f8de666ef03e
Coder-Ankit001 <ankitchaudhary7157@gmail.com>
2026-07-03 08:42:14 +0000

    Redirect user from root to api/books

---
+++ akashic_records/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.http import HttpResponse
+
+def home(request):
+    if request.method == 'GET':
+        return redirect(reverse('books:user_book_list'))
+    return HttpResponse('<h1> Welcome to Home Page </h1>')
